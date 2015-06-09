c2458831aa65697586340efbe12a4a4bf28f3ff3
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-06-09 13:50:25 -0400

    Exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,19 @@
+# Author: Kevin McLoughlin
+# Exercise 4
+
+one_to_100 = (1..100)
+
+one_to_100.each do |number|
+	if number % 3 == 0 && number % 5 == 0
+		puts "BitMaker"
+	elsif number % 3 == 0
+		puts "Bit"
+	elsif number % 5 == 0
+		puts "Maker"
+	else
+		puts number
+	end
+end
+puts ""
+puts "Hooray for a modified version of the infamous \"FizzBuzz\" challenge!"
+		
